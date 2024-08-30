786616964997956a3be99e0652aadd5514670842
AlexandraXV <fkbyf1999@rambler.ru>
2024-08-30 16:37:25 +0300

    diplom

--- test_ui.py
+++ test_ui.py
@@ -1,9 +1,7 @@
 from ui import ui
 
-from time import sleep
-
 from selenium import webdriver
-
+from time import sleep
 
 import allure
 
@@ -16,27 +14,37 @@ ui_page = ui(driver)
 @allure.severity(allure.severity_level.CRITICAL)
 def test_find_movie():
     ui_page.find_movie("The Crow")
-
+    result = ui_page.find_header_movie()
+    assert result == 'Ворон'
+    driver.quit()
 
 @allure.title("Кнопка Телеканалы")
 @allure.description("Тестирование кнопки Телеканалы. Проверка функциональности кнопок внутри вкладки.")
 @allure.severity(allure.severity_level.CRITICAL)
 def test_channels():
     ui_page.channels()
-
+    result = ui_page.find_header_channels()
+    assert result == "Смотреть каналы"
 
 @allure.title("Возрат на главную страницу")
 @allure.description("Переход на вкладку, нажатие на кнопку Кинопоиск для возврата на главную страницу.")
 @allure.severity(allure.severity_level.CRITICAL)
 def test_back_main():
     ui_page.back_in_main_screen()
-
+    result = ui_page.find_main_header()
+    """
+    Утверждающая функция пустая, так как слово 
+    Кинопоиск на сайт сделан в картинке, а не написан.
+    """
+    assert result == ""
 
 @allure.title("Просмотр трейлера")
 @allure.description("Поиск фильма через поисковик, нажатие на кнопку Трейлер.")
 @allure.severity(allure.severity_level.CRITICAL)
 def test_watching_the_trailer():
     ui_page.watching_the_trailer('The Avengers')
+    result = ui_page.find_header_trailer()
+    assert result == "Трейлеры"
 
 
 @allure.title("Использвание фильтра")
@@ -44,3 +52,5 @@ def test_watching_the_trailer():
 @allure.severity(allure.severity_level.CRITICAL)
 def test_filters():
     ui_page.filters('Steven Spielberg')
+    result = ui_page.find_header_filters()
+    assert result == 'Стивен Спилберг'
--- ui.py
+++ ui.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 import allure
 
 
+
 class ui:
 
     def __init__(self, driver):
@@ -12,7 +13,6 @@ class ui:
             "https://www.kinopoisk.ru/?from=tableau_yabro")
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
-        driver.quit()
 
     @allure.step('Закрывает всплывающее окно с рекламой.')
     def close_window(self):
@@ -26,6 +26,10 @@ class ui:
             '.styles_input__4vNAb.kinopoisk-header-search-form-input__input'
         ).send_keys(movie, Keys.ENTER)
 
+    @allure.step("Ищет совпадение по наименованию фильма")
+    def find_header_movie(self):
+        return self.driver.find_element(By.XPATH, '//*[@id="block_left_pad"]/div/div[2]/div/div[2]/p/a').text
+
     @allure.step("Проверка функциональности кнопок во вкладке каналы.")
     def channels(self):
         self.driver.find_element(
@@ -33,7 +37,7 @@ class ui:
         ).click()
 
         self.driver.find_element(
-            By.CSS_SELECTOR, '//*[@id="__next"]/div[1]/div[2]/div[2]/div/div/div[1]/div/div/div/nav/ul/li[5]/a'
+            By.XPATH, '//*[@id="__next"]/div[2]/div[2]/div/div/ul/li[2]/button'
         ).click()
         self.driver.find_element(
             By.XPATH, '//*[@id="__next"]/div[2]/div[2]/div/div/ul/li[3]/button').click()
@@ -50,12 +54,20 @@ class ui:
         self.driver.find_element(
             By.XPATH, '//*[@id="__next"]/div[2]/div[2]/div/div/ul/li[9]/button').click()
 
+    @allure.step("Ищет заголовок вкладки в телеканалах")
+    def find_header_channels(self):
+        return self.driver.find_element(By.XPATH, '//*[@id="__next"]/div[2]/div[2]/div/div/div[1]/h1').text
+
     @allure.step("Возращение на главный экран через кнопку.")
     def back_in_main_screen(self):
         self.driver.find_element(
             By.XPATH, '//*[@id="__next"]/div[1]/div[2]/div[2]/div/div/div[1]/div/div/div/nav/ul/li[4]/a').click()
         self.driver.find_element(
             By.CSS_SELECTOR, '.styles_img__3hWmL.kinopoisk-header-logo__img').click()
+
+    @allure.step("Ищет заголовок главной страницы")
+    def find_main_header(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '.styles_img__3hWmL.kinopoisk-header-logo__img').text
 
     @allure.step("Просмотр трейлера {value} через поисковик.")
     def watching_the_trailer(self, value):
@@ -65,11 +77,20 @@ class ui:
         ).send_keys(value, Keys.ENTER)
         self.driver.find_element(
             By.XPATH, '//*[@id="block_left_pad"]/div/div[2]/div/div[1]/ul/li[2]/a').click()
+        self.driver.find_element(By.XPATH, '//*[@id="tr_first_t61544"]/table[2]/tbody/tr[1]/td/table/tbody/tr/td/div/div/div').click()
+
+    @allure.step("Поиск слова Трейлеры")
+    def find_header_trailer(self):
+        return self.driver.find_element(By.CSS_SELECTOR, '.breadcrumbs__link').text
 
     @allure.step("Поиск актера/режиссера {name} через  фильтр.")
     def filters(self, name):
         self.driver.find_element(
-            By.XPATH, '//*[@id="__next"]/div[1]/div[1]/header/div/div[2]/div[2]/div/form/div/div/a/svg/path').click()
-        self.driver.find_element(By.CSS_SELECTOR, '.text.el_1').send_keys(name)
+            By.CSS_SELECTOR, '.styles_advancedSearchIcon__Zxjax').click()
+        self.driver.find_element(By.CSS_SELECTOR, '#find_people').send_keys(name)
         self.driver.find_element(
             By.CSS_SELECTOR, '.el_8.submit.nice_button').click()
+
+    @allure.step("Поиск заголовка найденного человека")
+    def find_header_filters(self):
+        return self.driver.find_element(By.XPATH, '//*[@id="block_left_pad"]/div/div[2]/div/div[2]/p/a').text
